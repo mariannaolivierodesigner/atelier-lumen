b3e90a8c6d61e5bf4aa902803ea323b15fa4d0ea
mariannaolivierodesigner <mariannaolivierodesigner@gmail.com>
2026-09-10 18:19:31 +0200

    Update catalog.server.ts

--- src/lib/catalog.server.ts
+++ src/lib/catalog.server.ts
@@ -95,7 +95,6 @@ export const catalogImportSchema = z.object({
 });
 
 const dateSchema = z.string().regex(/^\d{4}-\d{2}-\d{2}$/, "Data non valida");
-const timeSchema = z.string().regex(/^([01]\d|2[0-3]):[0-5]\d$/, "Orario non valido");
 
 export const closureInputSchema = z
   .object({
